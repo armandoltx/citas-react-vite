e77c36f1f53b84f6868e810634d2c5db471610f1
Armando Ruiz <armando.ruiz@industrie.co>
2022-03-21 12:31:41 +1100

    72. Mostrando el mensaje de error si los campos estan vacios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,12 +6,20 @@ const Formulario = () => {
   const [ email, setEmail ] = useState('');
   const [ fecha, setFecha ] = useState('');
   const [ sintomas, setSintomas ] = useState('');
+  const [ error, setError ] = useState(false);
 
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("submited");
+    if([ nombre, propietario, email, fecha, sintomas ].includes('')) {
+      console.log('Algun campo esta vacio');
+
+      setError(true);
+      return;
+    }
+
+    setError(false);
   }
 
 
@@ -29,6 +37,12 @@ const Formulario = () => {
       onSubmit={handleSubmit}
         className='bg-white shadow-md rounded-lg py-10 px-5'
       >
+        { error &&
+          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+            <p>Todos los campos son obligatorios.</p>
+          </div>
+        }
+
         <div className='mb-5'>
           <label htmlFor="mascota" className='block text-gray-700 uppercase font-bold'>Nombre Mascota</label>
           <input
