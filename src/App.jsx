5b9978ae5f8ffa6935ffbe081748b51b0a9891a4
Armando Ruiz <armando.ruiz@industrie.co>
2022-05-05 10:19:13 +1000

    86. Eliminando un paciente

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,16 @@ function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
+  const eliminarPaciente = (id) => {
+    // console.log('Eliminando el paciente ', id);
+
+    // uso filter y creo un nuevo arreglo donde me traigo todos los pacientes excepto el del id igual.
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id )
+
+    // console.log(pacientesActualizados);
+    setPacientes(pacientesActualizados);
+  }
+
   return (
     <div className='container mx-auto mt-20'>
       <Header />
@@ -21,6 +31,7 @@ function App() {
         <ListadoPacientes
           pacientes={pacientes}
           setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
         />
       </div>
     </div>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,5 +1,14 @@
-const Paciente = ({paciente, setPaciente}) => {
-  const { nombre, propietario, email, fecha, sintomas } = paciente
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
+  const { nombre, propietario, email, fecha, sintomas, id } = paciente
+
+  const handleEliminar = () => {
+    const respuesta = confirm('Deseas eliminar este paciente?');
+
+    if(respuesta) {
+      eliminarPaciente(id);
+    }
+  }
+
   return (
     <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
@@ -34,6 +43,7 @@ const Paciente = ({paciente, setPaciente}) => {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white fonr-bold uppercase rounded-lg"
+          onClick={handleEliminar}
         >
           Eliminar
         </button>
